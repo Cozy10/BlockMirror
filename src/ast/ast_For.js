3798f35c14decaaab3db2ac9735dbb20275596e2
Romain FLEURETTE <romainfleurette@pm.me>
2022-02-22 22:16:45 +0100

    for in range

--- src/ast/ast_For.js
+++ src/ast/ast_For.js
@@ -1,49 +1,3 @@
-BlockMirrorTextToBlocks.BLOCKS.push({
-  "type": "ast_For",
-  "message0": "for each item %1 in list %2 : %3 %4",
-  "args0": [
-    { "type": "input_value", "name": "TARGET" },
-    { "type": "input_value", "name": "ITER" },
-    { "type": "input_dummy" },
-    { "type": "input_statement", "name": "BODY" }
-  ],
-  "inputsInline": true,
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": BlockMirrorTextToBlocks.COLOR.CONTROL,
-})
-
-BlockMirrorTextToBlocks.BLOCKS.push({
-  "type": "ast_ForElse",
-  "message0": "for each item %1 in list %2 : %3 %4 else: %5 %6",
-  "args0": [
-    { "type": "input_value", "name": "TARGET" },
-    { "type": "input_value", "name": "ITER" },
-    { "type": "input_dummy" },
-    { "type": "input_statement", "name": "BODY" },
-    { "type": "input_dummy" },
-    { "type": "input_statement", "name": "ELSE" }
-  ],
-  "inputsInline": true,
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": BlockMirrorTextToBlocks.COLOR.CONTROL,
-})
-
-Blockly.Python['ast_For'] = function(block) {
-  // For each loop.
-  var argument0 = Blockly.Python.valueToCode(block, 'TARGET',
-      Blockly.Python.ORDER_RELATIONAL) || Blockly.Python.blank;
-  var argument1 = Blockly.Python.valueToCode(block, 'ITER',
-      Blockly.Python.ORDER_RELATIONAL) || Blockly.Python.blank;
-  var branchBody = Blockly.Python.statementToCode(block, 'BODY') || Blockly.Python.PASS;
-  var branchElse = Blockly.Python.statementToCode(block, 'ELSE');
-  var code = 'for ' + argument0 + ' in ' + argument1 + ':\n' + branchBody;
-  if (branchElse) {
-      code += 'else:\n' + branchElse;
-  }
-  return code;
-};
 
 BlockMirrorTextToBlocks.prototype['ast_For'] = function (node, parent) {
     var target = node.target;
@@ -52,19 +6,52 @@ BlockMirrorTextToBlocks.prototype['ast_For'] = function (node, parent) {
     var orelse = node.orelse;
     
     var blockName = 'controls_forEach';
-    var bodies = {'BODY': this.convertBody(body, node)};
-    
+    var bodies = {'DO': this.convertBody(body, node)};
+    var iter_val;
+    console.log("C'est moi===============================");
+    // for i in range(...)
+    if (iter.func != undefined && iter.func.id.v === "range"){
+      console.log(node.iter)
+      // "for i in range(x)" block repeat x times
+      if(iter.args.length == 1){
+        blockName = "controls_repeat_ext";
+        iter_val = {
+          "TIMES": this.convert(iter.args[0], node)
+        };
+      }
+      // for i in range(x, y, step)
+      else{
+        // "for i in range(x, y)"
+        var by_block;
+        if(iter.args.length == 2){
+          by_block = BlockMirrorTextToBlocks.create_block("math_number", node.lineno, {
+              "NUM": 1
+            });
+        }
+        else{
+          by_block = this.convert(iter.args[2], node);
+        }
+        blockName = "controls_for";
+        iter_val = {
+          "FROM": this.convert(iter.args[0], node),
+          "TO": this.convert(iter.args[1], node),
+          "BY": by_block
+        }
+      }
+    }
+    else{
+      iter_val = {
+          "ITER": this.convert(iter, node),
+          "TARGET": this.convert(target, node)
+      };
+    }
     if (orelse.length > 0) {
         blockName = "ast_ForElse";
         bodies['ELSE'] = this.convertBody(orelse, node);
     }
 
     return BlockMirrorTextToBlocks.create_block(blockName, node.lineno, {
-    }, {
-        "ITER": this.convert(iter, node),
-        "TARGET": this.convert(target, node)
-    }, {}, {}, bodies);
+    }, iter_val, {}, {}, bodies);
 }
 
-Blockly.Python['ast_ForElse'] = Blockly.Python['ast_For'];
 BlockMirrorTextToBlocks.prototype['ast_ForElse'] = BlockMirrorTextToBlocks.prototype['ast_For'];
