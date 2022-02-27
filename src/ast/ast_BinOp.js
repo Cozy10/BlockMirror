f6b2a8a5e5b210463b5c51c8a8f1d9c30788535f
AntoineEsriva <antoine.200004@gmail.com>
2022-02-27 15:24:28 +0100

    Ajout math.js

--- src/ast/ast_BinOp.js
+++ src/ast/ast_BinOp.js
@@ -1,15 +1,3 @@
-BlockMirrorTextToBlocks.prototype.FUNCTIONS_BLOCKS["sqrt"] = function(args, node){ // Give node.args and node
-    return {
-        "name":"math_single", // block type="text_print"
-        "fields":{
-            "OP":"ROOT"
-        },        // tag field of the block <field ...>
-        "values":{          
-            "NUM":BlockMirrorTextToBlocks.prototype.convert(args[0], node) // recursive conversion for args[0]
-        },                  // tag value
-        "statements":{}     //tag statement
-    };
-};
 BlockMirrorTextToBlocks.CONVERT_BINOPS = { 
     "Sub" : "MINUS",
     "Add" : "ADD",
