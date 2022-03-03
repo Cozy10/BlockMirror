cbe4675441883605ff6d66933be3457c2ec653e7
Romain FLEURETTE <romainfleurette@pm.me>
2022-03-03 14:51:41 +0100

    import_module basic

--- module_importer.js
+++ module_importer.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 const create_block_func = "BlockMirrorTextToBlocks.prototype.FUNCTIONS_BLOCKS";
 const block_converter = "BlockMirrorTextToBlocks.prototype.convert";
 const func_name = "test";
@@ -17,21 +17,30 @@ fs.readdir(modules_path, (err, modules)=>{
                 fs.readFile(path.join(json_path, file), (err, data) => {
                     if (err) throw err;
                     let block = JSON.parse(data);
-                    console.log(block);
-                    let content = create_block_func+'["'+func_name+'"]'+'=function(args,node){return{';
-                    content+='"name":"'+block.type+'",';
-                    content+='"values":{';
-                    block.args0.forEach((arg, i) => {
-                        content+='"'+arg.name+'":'+block_converter+'(args['+i+'],node),';
-                    });
-                    content+='},';
-                    content+="}}";
-                    fs.writeFile(path.join(import_path, file), content,{flag: 'w'}, err => {
+                    let func_name = file.split('.')[0];
+                    let code = genJSCode(func_name, block);                    
+                    fs.writeFile(path.join(import_path, func_name+'.js'), code,{flag: 'w'}, err => {
                         if (err) throw err;
-                    })
+                    });
                 });
             });
         })
-
     });
 })
+
+function genJSCode(func_name, block){
+    let content = create_block_func+'["'+func_name+'"]'+'=function(args,node){return{';
+    content+='"name":"'+block.type+'",';
+    content+=genValues(block.args0);
+    
+    content+="}}";
+    return content;
+}
+function genValues(args){
+    let code = '"values":{';
+    args.forEach((arg, i) => {
+        code+='"'+arg.name+'":'+block_converter+'(args['+i+'],node),';
+    });
+    code+='},';
+    return code;
+}
