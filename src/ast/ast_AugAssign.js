452b6c2ec8eb7a7fe982fe87f76de8710ed06855
Romain FLEURETTE <romainfleurette@pm.me>
2022-03-21 19:32:41 +0100

    redirect += to assign and Binop

--- src/ast/ast_AugAssign.js
+++ src/ast/ast_AugAssign.js
@@ -1,28 +1,10 @@
 PyBlock.prototype['ast_AugAssign'] = function (node, parent) {
-    let target = node.target;
-    let op = node.op.name;
-    let value = node.value;
-
-    let values = {'VALUE': this.convert(value, node)};
-    let fields = {'OP_NAME': op};
-    let simpleTarget = target._astname === 'Name';
-    if (simpleTarget) {
-        fields['VAR'] = Sk.ffi.remapToJs(target.id);
-    } else {
-        values['TARGET'] = this.convert(value, node);
-    }
-
-    let preposition = op;
-
-    let allOptions = BINOPS_SIMPLE.indexOf(op) === -1;
-
-    return PyBlock.create_block("ast_AugAssign", node.lineno, undefined, fields,
-        values,
-        {
-            "inline": "true",
-        }, {
-            "@options": allOptions,
-            "@simple": simpleTarget,
-            "@preposition": preposition
-        });
+    let node_BinOp = {...node};
+    node_BinOp.left = node.target;
+    node_BinOp.right = node.value;
+    node_BinOp._astname = "BinOp";
+    node_BinOp._parent = node;
+    node.value = node_BinOp;
+    node.targets = [node.target];
+    return PyBlock.prototype.ast_Assign(node, parent);
 };
